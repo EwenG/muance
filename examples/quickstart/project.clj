edff3eefcade4cdc4f95210077ab95abb90f3845
ewen <ewen.grosjean@free.fr>
2017-04-05 23:13:02 +0200

    clojure/script dependencies as :provided

--- examples/quickstart/project.clj
+++ examples/quickstart/project.clj
@@ -1,5 +1,11 @@
 (defproject muance/quickstart "0.0.1"
   :source-paths ["src"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [muance/muance "0.0.1"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [org.clojure/clojurescript "1.9.494"]
+                 [muance/muance "0.0.1"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :cljsbuild {:builds
+              [{:source-paths ["src"]
+                :compiler {:output-to "target/cljs/quickstart-o.js"
+                           :optimizations :advanced
+                           :pretty-print false}}]})
--- project.clj
+++ project.clj
@@ -1,13 +1,6 @@
 (defproject muance/muance "0.0.1"
   :source-paths ["src"]
   :test-paths ["test"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.293"]]
-  :plugins [[lein-cljsbuild "1.1.5"]]
-  
-  ;; An advanced compilation setup to check the muance bundle size
-  :cljsbuild {:builds
-              [{:source-paths ["examples/quickstart/src"]
-                :compiler {:output-to "target/cljs/quickstart-o.js"
-                           :optimizations :advanced
-                           :pretty-print false}}]})
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.494" :scope "provided"]]
+  :plugins [[lein-cljsbuild "1.1.5"]])
