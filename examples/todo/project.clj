c6f5410a88f0016a4be9e014eed6b08778968f91
ewen <ewen.grosjean@free.fr>
2017-04-08 00:19:36 +0200

    remove lein-cljsbuild from project.clj

--- examples/todo/project.clj
+++ examples/todo/project.clj
@@ -2,4 +2,10 @@
   :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/clojurescript "1.9.293"]
-                 [muance/muance "0.0.1"]])
+                 [muance/muance "0.0.1"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :cljsbuild {:builds
+              [{:source-paths ["src"]
+                :compiler {:output-to "target/cljs/todo.min.js"
+                           :optimizations :advanced
+                           :pretty-print false}}]})
