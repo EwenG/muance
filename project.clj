d79f6fb8abe2d66901a18ec4a31314173c64989a
ewen <ewen.grosjean@free.fr>
2017-05-01 14:23:54 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject muance/muance "0.0.1"
   :source-paths ["src"]
   :test-paths ["test"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.494" :scope "provided"]]
+  :dependencies []
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                                  [org.clojure/clojurescript "1.9.521"]
+                                  [org.clojure/test.check "0.9.0"]]}}
   :plugins [[lein-cljsbuild "1.1.5"]])
