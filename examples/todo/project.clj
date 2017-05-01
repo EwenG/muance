81d9cbad2fb69c74a7c6d06e43d2772d6f546674
ewen <ewen.grosjean@free.fr>
2017-05-01 19:19:58 +0200

    todo example

--- examples/todo/project.clj
+++ examples/todo/project.clj
@@ -1,7 +1,7 @@
 (defproject muance/todo "0.0.1"
   :source-paths ["src"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.293"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [org.clojure/clojurescript "1.9.521"]
                  [muance/muance "0.0.1"]]
   :plugins [[lein-cljsbuild "1.1.5"]]
   :cljsbuild {:builds
