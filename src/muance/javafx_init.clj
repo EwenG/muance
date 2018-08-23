e3f9a6fe6a91a29ffcc23b08abe5010899776cb9
ewen <ewen.grosjean@free.fr>
2018-08-23 22:56:38 +0200

    javafx_init thread

--- src/muance/javafx_init.clj
+++ src/muance/javafx_init.clj
@@ -1,14 +1,15 @@
 (ns muance.javafx-init
   (:import [javafx.application Application]))
 
+(defn- lauch-app []
+  (Application/launch muance.javafx.MuanceFxApp (make-array String 0)))
+
 (defn start-app []
   (when-not (realized? muance.javafx.MuanceFxApp/stagePromise)
-    (let [app-thread (proxy [Thread] []
-                       (run [] (Application/launch
-                                muance.javafx.MuanceFxApp
-                                (make-array String 0))))]
-      (.start app-thread)
-      @muance.javafx.MuanceFxApp/stagePromise)))
+    (doto (Thread. ^Runnable lauch-app)
+      (.setDaemon false)
+      (.start))
+    @muance.javafx.MuanceFxApp/stagePromise))
 
 ;; Some Classes cannot be loaded before the javax platform is initialized. It is initialized by start-app.
 
