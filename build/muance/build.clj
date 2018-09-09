ed2bf89b0ebc6509188e31b19419c695ced2f6e8
ewen <ewen.grosjean@free.fr>
2018-09-09 15:30:01 +0200

    build script

--- build/muance/build.clj
+++ build/muance/build.clj
@@ -1,13 +1,23 @@
 (ns muance.build
   (:require [badigeon.jar :as jar]
-            [badigeon.install :as install]))
+            [badigeon.install :as install]
+            [badigeon.javac :as javac]
+            [badigeon.clean :as clean]
+            [clojure.tools.deps.alpha.util.maven :as maven]
+            [clojure.tools.deps.alpha.extensions :as ext]))
 
-(defn -main []
+(def clojure-path (first (ext/coord-paths 'org.clojure/clojure {:mvn/version "1.9.0"} :mvn {:mvn/repos maven/standard-repos})))
+
+(defn build []
+  (clean/clean "target")
   (let [lib 'muance/muance-aot
         coords {:mvn/version "0.0.1-SNAPSHOT"}
+        _ (javac/javac "src-java" {:javac-options ["--module-path" (str "javafx-sdk-11/lib:" clojure-path) "-deprecation" "-g:none" "-source" "10" "-target" "10"]})
         jar-file (jar/jar lib coords
                           {:paths ["target/classes"]
                            :deps {}})]
     (install/install lib coords jar-file "pom.xml")))
 
-
+(defn -main []
+  (build)
+  (shutdown-agents))
