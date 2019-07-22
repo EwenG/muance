c9ffd0f2f11d453d3c9f1fb0a07ed1204c33a119
ewen <ewen.grosjean@free.fr>
2019-07-23 00:21:56 +0200

    remove project.clj

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject muance/muance "0.0.1"
-  :source-paths ["src"]
-  :test-paths ["test"]
-  :dependencies []
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha15"]
-                                  [org.clojure/clojurescript "1.9.521"]
-                                  [org.clojure/test.check "0.9.0"]]}}
-  :plugins [[lein-cljsbuild "1.1.5"]])
